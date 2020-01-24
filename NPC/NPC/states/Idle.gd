bdec928c6afe44c96394633a3ed776eca5560b54
intey <ziexe0@gmail.com>
2020-01-24 18:18:45 +0300

    roaming of NPC for testing it's battle

--- NPC/NPC/states/Idle.gd
+++ NPC/NPC/states/Idle.gd
@@ -1,6 +1,8 @@
 extends "res://states/state.gd"
 
 
+var target_direction 
+
 func update_impl(delta):
     if self.host.view_enemy():
         self.host.nearest_enemy = self.host.visible_enemies[0]
@@ -11,3 +13,51 @@ func update_impl(delta):
                 min_pos = nd
                 self.host.nearest_enemy = enemy
         return self.host.PURSUIT_STATE
+
+func physics_process_impl(delta):
+    var host = self.host
+    target_direction = self.select_direction()
+    # TODO: remember path or came_from point, or last point. 
+    # move until not find other farest_direction in other direction
+    var velocity_dir = (target_direction - host.global_position).normalized()
+    self.host.move_and_slide(velocity_dir * host.speed)
+
+func select_direction() -> Vector2:
+    return find_farest_visible_direction()
+    
+    
+func find_farest_visible_direction() -> Vector2:
+    var host = self.host
+    # var space_rid = get_world_2d().space
+    # var space_state = Physics2DServer.space_get_direct_state(space_rid)
+    var space_state = host.get_world_2d().direct_space_state
+    var excepts = [self]
+    var max_direction = Vector2(cos(0), sin(0))
+    var max_distance = 0
+    
+    var max_direction_angle = 0
+    # RayCast на каждые 5 градусов, чтобы найти самую дальнюю позицию
+    for angle in range(0, 360, 5):
+        var cast_to = Vector2(cos(angle), sin(angle))        
+        var result = space_state.intersect_ray(host.global_position, cast_to, excepts)
+        # если есть коллизия - проверяем дистанцию до объекта 
+        if result:
+            result = result.collider
+            var distance = host.global_position.distance_to(result.global_position)
+            if distance > max_distance:
+                max_direction_angle = angle
+                # print_debug("angle for farest direction ", angle)
+                max_direction = result.global_position
+                max_distance = distance
+    
+    # чтобы не ходить вперед-назад
+    if target_direction != null:
+        var angle = max_direction.dot(target_direction)
+         # смотрим что новая позиция перед нами(в секторе на 180%)
+        if angle > 0:
+            return max_direction
+    # Если где-то за нами - продолжаем идти вперед
+        else:
+            return target_direction
+    else:
+        return max_direction
