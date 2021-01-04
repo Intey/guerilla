12430b7764570b7d061f7173da79927aac3cbad8
intey <ziexe0@gmail.com>
2021-01-04 12:11:12 +0300

    [fix]: can't detect that subtract done

--- Player/Inventory.gd
+++ Player/Inventory.gd
@@ -21,11 +21,13 @@ func add(collected: ResourceItem):
     emit_signal('update', self.inventory)
     
     
-func subtract(res: ResourceItem):
-    if self.inventory[res.name] < res.count:
+func subtract(res: ResourceItem) -> bool:
+    print_debug("try substract ", res.name, ":", res.count)
+    if self.inventory.get(res.name, 0) < res.count:
         return false
         
     self.inventory[res.name] -= res.count
     if self.inventory[res.name] == 0:
         self.inventory.erase(res.name)
     emit_signal("update", self.inventory)
+    return true
